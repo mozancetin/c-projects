82b21acf491456bbbd52c352bcd02a10250a2dfa
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2021-03-25 16:17:18 +0300

    added even fibonacci sum

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int fib1 = 1;
+    int fib2 = 1;
+    int t = 0;
+    int n;
+
+    printf("Enter the number n\n");
+    scanf("%d", &n);
+
+    while(1){
+        int fib = fib1 + fib2;
+        if(fib % 2 == 0){
+            t += fib;
+        }
+
+        if(fib > n){
+            printf("\nSum of all even fibonacci numbers less than %d: %d", n, t);
+            break;
+        }
+        else{
+            fib1 = fib2;
+            fib2 = fib;
+        }
+
+    }
+
+
+}
